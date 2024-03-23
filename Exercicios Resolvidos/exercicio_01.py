0d2b7706931892188dbcb70fd502d2a2cc59ad92
Jonathan Moraes <jonamoraes20@gmail.com>
2024-03-23 19:32:07 -0300

    Adicionar exercicio_01 resolvido

---
+++ Exercicios Resolvidos/exercicio_01.py
@@ -0,0 +1,8 @@
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+numero_01 = int(input("Inserir um número inteiro: "))
+numero_02 = int(input("Inserir outro número inteiro: "))
+
+resultado = numero_01 + numero_02
+
+print(resultado)
