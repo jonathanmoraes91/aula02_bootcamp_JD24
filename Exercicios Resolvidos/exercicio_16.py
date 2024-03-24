029a509329938fdb2eb9041acdc2030d89ac3574
Jonathan Moraes <jonamoraes20@gmail.com>
2024-03-24 20:08:59 -0300

    Adicionar Exercicio 16 Resolvido

---
+++ Exercicios Resolvidos/exercicio_16.py
@@ -0,0 +1,8 @@
+# 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+
+valor_01 = True
+valor_02 = False
+
+Resultado = valor_01 and valor_02
+
+print(f"Resultado do AND logico: {Resultado}")
