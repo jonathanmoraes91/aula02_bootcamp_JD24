fd2712dcb8a800bbafaa921f76dc64b7d59f0372
Jonathan Moraes <jonamoraes20@gmail.com>
2024-03-24 07:14:56 -0300

    Adicionar exercicio_12 resolvido

---
+++ Exercicios Resolvidos/exercicio_12.py
@@ -0,0 +1,6 @@
+# 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+
+nome_completo = str(input("Digite o seu nome completo com letras maiúsculas: "))
+nome_minusculas = nome_completo.lower()
+
+print(f"Nome com letras minusculas: {nome_minusculas}")
