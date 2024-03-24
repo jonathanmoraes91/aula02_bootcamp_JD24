857c4cf1be317dcd990b3a43a28cc43a55466e98
Jonathan Moraes <jonamoraes20@gmail.com>
2024-03-24 20:23:48 -0300

    Adicionar Exercicio 17  Resolvido

---
+++ Exercicios Resolvidos/exercicio_17.py
@@ -0,0 +1,8 @@
+# 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.]
+
+valor_01 = True
+valor_02 = False
+
+Resultado = valor_01 or valor_02
+
+print(f"Resultado do OR logico: {Resultado}")
