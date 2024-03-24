361dcf3131841c145c69d25b56bcc35c6afa934f
Jonathan Moraes <jonamoraes20@gmail.com>
2024-03-24 20:30:55 -0300

    Adicionar Exercicio 18 Resolvido

---
+++ Exercicios Resolvidos/exercicio_18.py
@@ -0,0 +1,7 @@
+# 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+
+valor_01 = bool(input("Digite um valor booleanod 'True or false': "))
+
+resultado_not = not valor_01
+
+print(f"Resultado do NOT lógico: {resultado_not}")
