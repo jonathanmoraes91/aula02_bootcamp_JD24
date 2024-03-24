5c282b3bb3094da1a4587d1efc0f5b22c8fafc42
Jonathan Moraes <jonamoraes20@gmail.com>
2024-03-24 07:26:52 -0300

    Adicionar exercicio_15 resolvido

---
+++ Exercicios Resolvidos/exercicio_15.py
@@ -0,0 +1,8 @@
+# 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+parte_texto_01 = str(input("Digite a primeira parte de um texto: "))
+parte_texto_02 = " "
+parte_texto_03 = str(input("Digite a segunda parte de um texto: "))
+
+texto_concatenado = parte_texto_01 + parte_texto_02 + parte_texto_03
+print(f"Texto completo: {texto_concatenado}")
