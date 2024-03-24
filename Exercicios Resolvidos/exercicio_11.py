a36d61d04020f200ca7b6dae93d74a7170b3a85a
Jonathan Moraes <jonamoraes20@gmail.com>
2024-03-24 07:06:49 -0300

    Adicionar exercicio_11 resolvido

---
+++ Exercicios Resolvidos/exercicio_11.py
@@ -0,0 +1,6 @@
+# 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+texto_01 = str(input("Digite um texto, vamos converter tudo em maiúsculo: "))
+texto_01_maiusculas = texto_01.upper()
+
+print(f"Texto com todas as letras maiúsculas: {texto_01_maiusculas}")
