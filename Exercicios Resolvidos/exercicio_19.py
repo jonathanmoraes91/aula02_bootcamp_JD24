c896a3c06f816d193980fa513dcf4ed03e805065
Jonathan Moraes <jonamoraes20@gmail.com>
2024-04-01 13:13:25 -0300

    Adicionar exercicio_19 resolvido

---
+++ Exercicios Resolvidos/exercicio_19.py
@@ -0,0 +1,7 @@
+# 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+
+
+num1 = int(5)
+num2 = int(5)
+resultado_igualdade = (num1 == num2)
+print("Resultado da igualdade:", resultado_igualdade)
