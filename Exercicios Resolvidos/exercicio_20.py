e285fcf456dcad9b1673dafa93e6d87de8b7ebae
Jonathan Moraes <jonamoraes20@gmail.com>
2024-04-01 13:18:54 -0300

    Adicionar exercicio_20 resolvido

---
+++ Exercicios Resolvidos/exercicio_20.py
@@ -0,0 +1,6 @@
+# 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+
+num1 = int(5)
+num2 = int(12)
+resultado_diferenca = (num1 != num2)
+print("Resultado da diferença:", resultado_diferenca)
