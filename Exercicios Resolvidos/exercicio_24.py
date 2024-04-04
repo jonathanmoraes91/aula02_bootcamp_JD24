ef0a7e4a626bc20c45d678d567281985fea6d0ad
Jonathan Moraes <jonamoraes20@gmail.com>
2024-04-04 07:24:58 -0300

    Adicionar exercicio 24 resolvido

---
+++ Exercicios Resolvidos/exercicio_24.py
@@ -0,0 +1,16 @@
+# 24: Classificador de Números
+
+try:
+    numero = int(input("Digite um número: "))
+    if numero > 0:
+        print("Positivo")
+    elif numero < 0:
+        print("Negativo")
+    else:
+        print("Zero")
+    if numero % 2 == 0:
+        print("Par")
+    else:
+        print("Ímpar")
+except ValueError:
+    print("Por favor, digite um número inteiro válido")
